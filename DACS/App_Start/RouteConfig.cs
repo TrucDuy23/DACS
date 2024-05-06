0f5175a35c425a19b3f4dc06283ae3beb035b2e6
Đức Mạnh Trần <manht260@gmail.com>
2024-05-06 23:28:40 +0700

    S

--- DACS/App_Start/RouteConfig.cs
+++ DACS/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace OnlineCourse
+namespace DACS
 {
     public class RouteConfig
     {
@@ -46,8 +46,8 @@ namespace OnlineCourse
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional},
-                namespaces: new[] { "OnlineCourse.Controllers"});
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "OnlineCourse.Controllers" });
         }
     }
 }
--- DACS/App_Start/WebApiConfig.cs
+++ DACS/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace OnlineCourse
+namespace DACS
 {
     public static class WebApiConfig
     {
