a5dad915f784c38fd152e67e488167191289655e
Đức Mạnh Trần <manht260@gmail.com>
2024-05-17 12:46:43 +0700

    u

---
+++ DACS/Areas/Admin/Controllers/QuestionController.cs
@@ -0,0 +1,99 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DACS.DAO;
+using DACS.Models;
+
+namespace DACS.Areas.Admin.Controllers
+{
+    public class QuestionController : BaseController
+    {
+        // GET: Admin/Question
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 200)
+        {
+            var dao = new QuestionDAO();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+        public void SetViewBag(long? selectedId = null)
+        {
+            var dao = new ProductDAO();
+            ViewBag.ProductList = dao.ListAllProduct();
+        }
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new QuestionDAO().Delete(id);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public JsonResult AddQuestionAjax(string name, string content, string answer, string productid)
+        {
+            try
+            {
+                var dao = new QuestionDAO();
+                Question question = new Question();
+
+
+                question.Name = name;
+                question.Content = content;
+                question.Answer = answer;
+                question.ProductID = Convert.ToInt16(productid);
+                question.Type = "1";              
+                question.Status = true;
+
+                long id = dao.Insert(question);
+                if (id > 0)
+                {
+                    return Json(new { status = true });
+                }
+                else
+                {
+                    return Json(new { status = false });
+                }
+            }
+            catch
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+        }
+        [HttpPost]
+        public JsonResult UpdateQuestionAjax(string id,string name, string content, string answer, string productid)
+        {
+            try
+            {
+                var dao = new QuestionDAO();
+                Question question = new Question();
+                question = dao.ViewDetail(Convert.ToInt16(id));
+                question.Name = name;
+                question.Content = content;
+                question.Answer = answer;
+                question.ProductID = Convert.ToInt16(productid);               
+                question.Status = true;
+
+                bool editquestion = dao.Update(question);
+                if (editquestion == true)
+                {
+                    return Json(new { status = true });
+                }
+                else
+                {
+                    return Json(new { status = false });
+                }
+            }
+            catch
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+        }
+    }
+}*/
---
+++ DACS/DAO/QuestionDAO.cs
@@ -0,0 +1,70 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DACS.Models;
+using PagedList;
+
+namespace DACS.DAO
+{
+    public class QuestionDAO
+    {
+        DBModel db = null;
+        public QuestionDAO()
+        {
+            db = new DBModel();
+        }
+    }
+    public long Insert(Question entity)
+    {
+        db.Questions.Add(entity);
+        db.SaveChanges();
+        return entity.ID;
+    }
+    public bool Delete(int id)
+    {
+        try
+        {
+            var question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+    public IEnumerable<Question> ListAllPaging(string searchString, int page, int pagesize)
+    {
+        IQueryable<Question> model = db.Questions;
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            model = model.Where(x => x.Content.Contains(searchString) || x.Name.Contains(searchString));
+        }
+        return model.OrderByDescending(x => x.ID).ToPagedList(page, pagesize);
+    }
+    public Question ViewDetail(int id)
+    {
+
+        return db.Questions.Find(id);
+    }
+    public bool Update(Question entity)
+    {
+        try
+        {
+            var question = db.Questions.Find(entity.ID);
+            question.Name = entity.Name;
+            question.Content = entity.Content;
+            question.Answer = entity.Answer;
+            question.ProductID = entity.ProductID;
+            db.SaveChanges();
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+
+        }
+    }
+}*/
